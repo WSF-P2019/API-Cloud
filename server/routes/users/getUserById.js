a0fc14f7ee08b72b06d1f506afc11a3342a11ebd
GuillaumeRZ <gui.remyzephir@gmail.com>
2018-02-14 10:19:25 +0100

    Add tittle library and convert try catch shit

--- server/routes/users/getUserById.js
+++ server/routes/users/getUserById.js
@@ -1,5 +1,6 @@
 const joi = require('joi');
 const db = require('../../utils/database');
+const tittle = require('tittle');
 
 module.exports = {
   method: 'GET',
@@ -12,17 +13,16 @@ module.exports = {
       }
   },
   handler: async function (req, handler) {
-    let user = [];
-    try{
-        user = await db.select().from('users').where({
-            id: req.params.id // We are passing an object to authorized multiple conditions
-        })
-    } catch (error) {
+
+    const query = db.select().from('users').where({id: req.params.id}) // We are passing an object to authorized multiple conditions
+    let [user, error] = await tittle(query);
+    if (error) {
         return handler.response({
             statusCode: 400,
             error: 'Something bad happened: + explicit error'
         }).code(400)
     }
+
     return handler.response({
         statusCode: 200,
         data: user[0]
--- server/routes/users/getUsers.js
+++ server/routes/users/getUsers.js
@@ -1,19 +1,21 @@
 const joi = require('joi');
 const db = require('../../utils/database');
+const tittle = require('tittle');
 
 module.exports = {
     method: 'GET',
     path: '/users',
     handler: async function (req, handler) {
-        let users = [];
-        try{
-            users = await db.select().from('users')
-        } catch (error) {
+
+        const query = db.select().from('users');
+        let [users, error] = await tittle(query);
+        if (error) {
             return handler.response({
                 statusCode: 400,
                 error: 'Something bad happened: + explicit error'
             }).code(400)
         }
+
         return handler.response({
             statusCode: 200,
             data: users
