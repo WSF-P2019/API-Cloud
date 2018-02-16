e7cadeb7d901b69f448059f7d394edd55fc2fcf5
GuillaumeRZ <gui.remyzephir@gmail.com>
2018-02-16 11:51:47 +0100

    Add settings folder

---
+++ server/settings/example.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  database: 'mysql://TO_COMPLETE:TO_COMPLETE@localhost:TO_COMPLETE/TO_COMPLETE',
+  http: {
+    host: 'TO_COMPLETE',
+    port: TO_COMPLETE,
+    routes: {
+      cors: {
+        origin: ['*']
+      }
+    }
+  }
+}
