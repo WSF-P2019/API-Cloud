e14332f902a7b72e69eb2e4415a5a7fd07e779e1
Guillaume Rémy-Zéphir <gui.remyzephir@gmail.com>
2018-02-16 12:42:55 +0100

    BRAVO ARTHUR. T'AS TOUT CASSE. BRAVO POUR L'APPAREIL PHOTO. 📸

--- server/plugins/token.js
+++ server/plugins/token.js
@@ -1,11 +1,9 @@
 module.exports = {
-    name: 'token',
-    version: '1.0.0',
-    register: (request, option) => {
-        console.log('request', request)
-        console.log('option', option)
-
-
-        request.on('request', )
-    }   
+  name: 'token',
+  version: '1.0.0',
+  register: (server, handler) => {
+    server.events.on('request', (req, res) => {
+      return res.continue;
+    });
+  }
 }
