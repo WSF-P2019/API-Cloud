43c8054746f4012b35fa358e4fca990437ca9ccc
GuillaumeRZ <gui.remyzephir@gmail.com>
2018-02-16 11:46:59 +0100

    Delete settings

--- server/settings/example.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  database: 'mysql://test:test@localhost:3306/test',
-  http: {
-    host: 'localhost',
-    port: 8000
-  }
-}
--- server/settings/production.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  database: 'mysql://root:roooot@localhost:3306/wsf',
-  http: {
-    host: process.env.HOST || process.env.HOSTNAME || 'localhost',
-    port: process.env.PORT || 8000
-  }
-}
